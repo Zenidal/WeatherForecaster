6f8d87b23916961314e1c343bf3665cde8b59102
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-03-21 22:56:18 +0300

    Added weatherAdapter.

--- Front-End/js/ng/ng.controllers.js
+++ Front-End/js/ng/ng.controllers.js
@@ -1,3 +1,22 @@
+function WeatherAdapter(){
+    return {
+        request:function(country, city,  weatherMain, weatherDescription, temp, humidity, pressure, temp_min, temp_max, 
+            windSpeed, windDeg){
+            return {
+                country: country,
+                city: city,
+                weatherMain: weatherMain,
+                weatherDescription: weatherDescription,
+                temp: temp,
+                humidity: humidity,
+                pressure: pressure, 
+                temp_min: temp_min,
+                temp_max: temp_max.
+            };
+        };
+    };
+};
+
 var contr = angular.module('app.controllers', [])
 
     .controller('WeatherAppController', [
@@ -8,7 +27,8 @@ var contr = angular.module('app.controllers', [])
 
     // Path: /Home
     .controller('HomeController', [
-             function ($scope, $rootScope,  $location) {
-        
+             function ($scope, $rootScope,  $location, weatherService) {
+            var adapter=new WeatherAdapter();
+
         }
     ]);
